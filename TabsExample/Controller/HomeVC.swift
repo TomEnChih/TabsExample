5965098491df9d00e3d890fc8a8285d56aa6740c
Tom EnChih <tom10485916@gmail.com>
2021-05-12 11:03:19 +0800

    新增 pageViewController 新增 pageViewController與CollectionView的聯動功能

---
+++ TabsExample/Controller/HomeVC.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  TabsExample
+//
+//  Created by 董恩志 on 2021/5/11.
+//
+
+import UIKit
+import SnapKit
+
+class HomeVC: UIViewController {
+    
+    let homeView = HomeView()
+    let pages = ["美食","電影","音樂"]
+    
+    var pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    var pageViewIndex: Int = 0
+    var viewControllers = [FoodVC(),MovieVC(),MusicVC()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = homeView
+        setCollectionView()
+        setPageViewController()
+    }
+    
+    // MARK: - Methods
+    func setCollectionView() {
+        homeView.tabsCollectionView.delegate = self
+        homeView.tabsCollectionView.dataSource = self
+        homeView.tabsCollectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: true, scrollPosition: .centeredHorizontally)
+    }
+    
+    func setPageViewController() {
+        view.addSubview(pageViewController.view)
+        addChild(pageViewController)
+        
+        pageViewController.view.snp.makeConstraints { (make) in
+            make.edges.equalTo(homeView.bottomView)
+        }
+        
+        pageViewController.delegate = self
+        pageViewController.dataSource = self
+        
+        
+        pageViewController.setViewControllers([viewControllers[pageViewIndex]], direction: .forward, animated: true)
+
+    }
+    
+    func pageViewToChange(index: Int) -> UIViewController? {
+        
+        if index < 0 {
+            return viewControllers.last
+            
+        } else if index > viewControllers.count-1 {
+            return viewControllers.first
+            
+        } else {
+            return viewControllers[index]
+        }
+    }
+    
+}
+
+//MARK: - CollectionViewDelegate,CollectionViewDataSource
+
+extension HomeVC: UICollectionViewDelegateFlowLayout,UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TabsCollectionViewCell.cellKey, for: indexPath) as! TabsCollectionViewCell
+        cell.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        cell.pageLabel.text = pages[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: homeView.tabsCollectionView.frame.width/4, height: homeView.tabsCollectionView.frame.height - 4)
+    }
+    
+    
+    //MARK: didSelect
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if indexPath.item > pageViewIndex {
+            pageViewIndex = indexPath.item
+            pageViewController.setViewControllers([viewControllers[pageViewIndex]], direction: .forward, animated: true)
+        }
+        if indexPath.item < pageViewIndex {
+            pageViewIndex = indexPath.item
+            pageViewController.setViewControllers([viewControllers[pageViewIndex]], direction: .reverse, animated: true)
+        }
+    }
+    
+}
+
+
+//MARK: - PageViewControllerDelegate, PageViewControllerDataSource
+
+extension HomeVC: UIPageViewControllerDelegate,UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        return pageViewToChange(index: pageViewIndex - 1)
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        return pageViewToChange(index: pageViewIndex + 1)
+        
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        if finished {
+            if completed {
+                guard let vc = pageViewController.viewControllers?.first else { return }
+                
+                pageViewIndex = vc.view.tag
+                
+                homeView.tabsCollectionView.selectItem(at: IndexPath(item: pageViewIndex, section: 0), animated: true, scrollPosition: .centeredVertically)
+            }
+        }
+    }
+    
+}
---
+++ TabsExample/Controller/MovieVC.swift
@@ -0,0 +1,35 @@
+//
+//  MovieVC.swift
+//  TabsExample
+//
+//  Created by 董恩志 on 2021/5/12.
+//
+
+import UIKit
+
+class MovieVC: UIViewController {
+
+    let label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.text = "Movie View"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        label.textAlignment = NSTextAlignment.center
+        return label
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tag = 1
+        view.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
+        
+    }
+    
+
+}
